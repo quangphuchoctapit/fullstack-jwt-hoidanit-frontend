afcd7e9b4c514fd9b30411cfcaef732f7ae14369
Fuc le <quangphuchoctapit@gmail.com>
2023-12-09 16:11:46 +0700

    state control

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Register.scss'
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 const Register = (props) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [rePassword, setRePassword] = useState('')
+    const [phone, setPhone] = useState('')
+    const [username, setUsername] = useState('')
+
     let history = useHistory()
     const handleGoToLogin = () => {
         history.push('/login')
+    }
+
+    const handleRegister = () => {
+        let userData = { email, username, password, phone }
+        console.log(userData)
     }
 
     useEffect(() => {
@@ -25,16 +36,16 @@ const Register = (props) => {
                     <div className='content-right py-3 col-12 col-sm-5 d-flex flex-column gap-2'>
                         <div className='brand d-block d-sm-none text-center fs-4 fw-bold'>Tommy Le</div>
                         <label htmlFor='email'>Enter your email</label>
-                        <input type='text' className='form-control' placeholder='Email' id='email' />
+                        <input value={email} onChange={e => setEmail(e.target.value)} type='text' className='form-control' placeholder='Email' id='email' />
                         <label htmlFor='phone'>Enter your phone number</label>
-                        <input type='text' className='form-control' placeholder='Phone number' id='phone' />
+                        <input value={phone} onChange={e => setPhone(e.target.value)} type='text' className='form-control' placeholder='Phone number' id='phone' />
                         <label htmlFor='username'>Enter your username</label>
-                        <input type='text' className='form-control' placeholder='Username' id='username' />
+                        <input value={username} onChange={e => setUsername(e.target.value)} type='text' className='form-control' placeholder='Username' id='username' />
                         <label htmlFor='password'>Enter your password</label>
-                        <input type='text' className='form-control' placeholder='Password' id='password' />
+                        <input value={password} onChange={e => setPassword(e.target.value)} type='text' className='form-control' placeholder='Password' id='password' />
                         <label htmlFor='re-password'>Confirm your password</label>
-                        <input type='text' className='form-control' placeholder='Re-enter your password' id='re-password' />
-                        <button className='mt-2 btn btn-primary'>Register</button>
+                        <input value={rePassword} onChange={e => setRePassword(e.target.value)} type='text' className='form-control' placeholder='Re-enter your password' id='re-password' />
+                        <button onClick={() => handleRegister()} className='mt-2 btn btn-primary'>Register</button>
                         <hr />
                         <div className='text-center'>
                             <button className='btn btn-success' onClick={() => handleGoToLogin()}>Already've an account. Login.</button>
