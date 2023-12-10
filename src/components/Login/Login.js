8aff28ca68a93482c226ec367bc62247c5be6bb7
Fuc le <quangphuchoctapit@gmail.com>
2023-12-10 11:46:06 +0700

    validate inputs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './Login.scss'
+import { useState } from 'react'
 import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify'
+
 
 const Login = (props) => {
     let history = useHistory()
     const handleCreateNewAccount = () => {
         history.push("/register")
+    }
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    let checkValidFunction = () => {
+        let requiredInputs = [email, password]
+        let requiredParams = ['Email or Phone', 'Password']
+        for (let i = 0; i < requiredInputs.length; i++) {
+            if (!requiredInputs[i]) {
+                toast.error(`${requiredParams[i]} is required`)
+                return false
+            }
+        }
+        return true
+    }
+    const handleLogin = () => {
+        let checkValid = checkValidFunction()
+        if (checkValid) {
+            toast.success('Ok Successfully logged in')
+        }
     }
     return (
         <div className='login-container pt-3'>
@@ -17,9 +41,11 @@ const Login = (props) => {
                     </div>
                     <div className='content-right d-flex flex-column col-12 col-sm-5 py-3 gap-3'>
                         <div className='d-sm-none d-sm-none text-center fs-3 fw-bold brand'>Tommy Le</div>
-                        <input placeholder='Enter email or phone number' type='text' className='form-control' />
-                        <input placeholder='Password' type='password' className='form-control' />
-                        <button className='btn btn-primary'>Login</button>
+                        <label htmlFor='email'>Enter an email or phone number</label>
+                        <input onChange={e => setEmail(e.target.value)} value={email} id='email' placeholder='Enter email or phone number' type='text' className='form-control' />
+                        <label htmlFor='password'>Enter your password</label>
+                        <input onChange={e => setPassword(e.target.value)} value={password} id='password' placeholder='Password' type='password' className='form-control' />
+                        <button className='btn btn-primary' onClick={() => handleLogin()}>Login</button>
                         <span className='text-center'><a href='#' className=' forgot-password'>Forgotten password?</a></span>
                         <hr />
                         <div className='text-center'>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -11,6 +11,14 @@ const Register = (props) => {
     const [rePassword, setRePassword] = useState('')
     const [phone, setPhone] = useState('')
     const [username, setUsername] = useState('')
+    const defaultValidInput = {
+        isValidEmail: true,
+        isValidPassword: true,
+        isValidRePassword: true,
+        isValidPhone: true,
+        isValidUsername: true
+    }
+    const [objCheckInput, setObjCheckInput] = useState(defaultValidInput)
 
     let history = useHistory()
     const handleGoToLogin = () => {
@@ -18,18 +26,50 @@ const Register = (props) => {
     }
 
     const checkValidInputs = () => {
+        setObjCheckInput(defaultValidInput)
+        if (!email) {
+            setObjCheckInput({ ...defaultValidInput, isValidEmail: false })
+            toast.error('Email is required')
+            return false
+        }
+        if (!phone) {
+            setObjCheckInput({ ...defaultValidInput, isValidPhone: false })
+            toast.error('Phone is required')
+            return false
+        }
+        if (!username) {
+            setObjCheckInput({ ...defaultValidInput, isValidUsername: false })
+            toast.error('Username is required')
+            return false
+        }
+        if (!password) {
+            setObjCheckInput({ ...defaultValidInput, isValidPassword: false })
+            toast.error('Password is required')
+            return false
+        }
+        if (!rePassword) {
+            toast.error('Confirm Password is required')
+            setObjCheckInput({ ...defaultValidInput, isValidRepassword: false })
+            return false
+        }
         let requiredInputs = [email, phone, username, password, rePassword]
         let requiredParams = ['Email', 'Phone', 'Username', "Password", 'Re-enter Password']
+        let validParam = ['isValidEmail', 'isValidPhone', 'isValidUsername', "isValidPassword", 'isValidRePassword']
         if (password !== rePassword) {
             toast.error('Passwords do not match')
             return false
         }
-        for (let i = 0; i < requiredInputs.length; i++) {
-            if (!requiredInputs[i]) {
-                toast.error(`${requiredParams[i]} input is required`)
-                return false
-            }
-        }
+        // for (let i = 0; i < requiredInputs.length; i++) {
+        //     if (!requiredInputs[i]) {
+        //         toast.error(`${requiredParams[i]} input is required`)
+        //         let valueParam = validParam[0]
+        //         console.log('check valei param: ', valueParam)
+        //         setObjCheckInput({ ...defaultValidInput, valueParam: false })
+        //         return false
+        //     }
+        // }
+
+
         return true
     }
 
@@ -37,15 +77,17 @@ const Register = (props) => {
         let checkValid = checkValidInputs()
         if (checkValid) {
             toast.success('OK Successfully Register')
+            axios.post('http://localhost:8080/api/v1/register', {
+                username, email, phone, password
+            })
         }
-        let userData = { email, username, password, phone }
-        console.log(userData)
     }
 
     useEffect(() => {
-        axios.get('http://localhost:8080/api/api-test').then((data) => {
+        axios.get('http://localhost:8080/api/v1/api-test').then((data) => {
             console.log('check data: ', data)
         })
+
     }, [])
     return (
         <div className='register-container'>
@@ -58,15 +100,15 @@ const Register = (props) => {
                     <div className='content-right py-3 col-12 col-sm-5 d-flex flex-column gap-2'>
                         <div className='brand d-block d-sm-none text-center fs-4 fw-bold'>Tommy Le</div>
                         <label htmlFor='email'>Enter your email</label>
-                        <input value={email} onChange={e => setEmail(e.target.value)} type='text' className='form-control' placeholder='Email' id='email' />
+                        <input value={email} onChange={e => setEmail(e.target.value)} type='text' className={objCheckInput.isValidEmail ? 'form-control' : 'form-control is-invalid'} placeholder='Email' id='email' />
                         <label htmlFor='phone'>Enter your phone number</label>
-                        <input value={phone} onChange={e => setPhone(e.target.value)} type='text' className='form-control' placeholder='Phone number' id='phone' />
+                        <input value={phone} onChange={e => setPhone(e.target.value)} type='text' className={objCheckInput.isValidPhone ? 'form-control' : 'form-control is-invalid'} placeholder='Phone number' id='phone' />
                         <label htmlFor='username'>Enter your username</label>
-                        <input value={username} onChange={e => setUsername(e.target.value)} type='text' className='form-control' placeholder='Username' id='username' />
+                        <input value={username} onChange={e => setUsername(e.target.value)} type='text' className={objCheckInput.isValidUsername ? 'form-control' : 'form-control is-invalid'} placeholder='Username' id='username' />
                         <label htmlFor='password'>Enter your password</label>
-                        <input value={password} onChange={e => setPassword(e.target.value)} type='text' className='form-control' placeholder='Password' id='password' />
+                        <input value={password} onChange={e => setPassword(e.target.value)} type='text' className={objCheckInput.isValidPassword ? 'form-control' : 'form-control is-invalid'} placeholder='Password' id='password' />
                         <label htmlFor='re-password'>Confirm your password</label>
-                        <input value={rePassword} onChange={e => setRePassword(e.target.value)} type='text' className='form-control' placeholder='Re-enter your password' id='re-password' />
+                        <input value={rePassword} onChange={e => setRePassword(e.target.value)} type='text' className={objCheckInput.isValidRePassword ? 'form-control' : 'form-control is-invalid'} placeholder='Re-enter your password' id='re-password' />
                         <button onClick={() => handleRegister()} className='mt-2 btn btn-primary'>Register</button>
                         <hr />
                         <div className='text-center'>
